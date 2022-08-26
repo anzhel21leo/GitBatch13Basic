60dbd682d147115e31de93e3936a557b71c9385b
Anzhelika Lysenko <anzhel56love@gmail.com>
2022-08-25 20:03:28 -0400

    temperature check basic code

---
+++ src/test/java/class01/gitBasics.java
@@ -0,0 +1,9 @@
+package class01;
+
+public class gitBasics {
+    public static void main(String[] args) {
+        //write down a code that checks the temp from website
+        //and then prints out beautiful day or not
+        System.out.println("Today is a beautiful day");
+    }
+}
