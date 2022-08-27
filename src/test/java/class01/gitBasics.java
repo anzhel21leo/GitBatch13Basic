2ba40f1405b92792066729c63c4b97dfa6afcf00
Anzhelika Lysenko <anzhel56love@gmail.com>
2022-08-27 10:45:22 -0400

    three lines of code

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -4,6 +4,8 @@ public class gitBasics {
     public static void main(String[] args) {
         //write down a code that checks the temp from website
         //and then prints out beautiful day or not
-        System.out.println("Today is a beautiful day");
+        System.out.println("Today is not beautiful day");
+        System.out.println("Today is not beautiful day");
+        System.out.println("Today is not beautiful day");
     }
 }
